76d9f7c491fb0cc6ac4d347f95177e6efa62896d
NguaGo <nguago@let.vn>
2015-04-22 15:53:14 +0700

    Update composer file

--- Cropper.php
+++ Cropper.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * Homepage: http://www.tinymce.com/
+ * Examples: http://www.tinymce.com/tryit/basic.php
+ * Options: http://www.tinymce.com/wiki.php/Configuration
+ * 
+ * Let Yii2 Tinymce v4.0.21 (Yii Framework 2.0 extention)
  * @copyright Copyright (c) 2014 Let.,ltd
  * @author Ngua Go <nguago@let.vn>
  */
@@ -9,7 +14,7 @@ use yii\helpers\Html;
 use yii\helpers\Json;
 use letyii\tinymce\TinymceAssets;
 
-class Cropper
+class Cropper extends \yii\base\Widget
 {
     public $src;
 
